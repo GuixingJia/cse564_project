5744a6b3e757dd35bc45761159b4ed5681e793f7
Guixingj <guixingj@asu.edu>
2025-12-02 19:00:01 -0700

    initial spring boot frame

---
+++ src/main/java/edu/asu/cse564/cse564_project/Cse564ProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.asu.cse564.cse564_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cse564ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cse564ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/asu/cse564/cse564_project/Cse564ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.asu.cse564.cse564_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cse564ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
